f5c538e872caa5d396198812c1d6fcc5a287f5aa
Gabriel dos Santos Garcia <gabriel.garcia@fortune.local>
2024-02-26 16:45:37 -0300

    first commit

---
+++ Suporte_TI_Garcia.Shared/Models/Arquitetura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suporte_TI_Garcia.Shared.Models
+{
+    public class Arquitetura
+    {
+        public string Empresa { get; set; }
+
+        public string Equipe {  get; set; }
+
+        public string NomeMaquina { get; set; }
+
+        public int PosicaoPa { get; set; }
+
+    }
+}
