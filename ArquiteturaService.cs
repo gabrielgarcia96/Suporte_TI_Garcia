5b3f77c03940242f41152778fac5a1045d641a6d
Gabriel Garcia <117041401+gabrielgarcia96@users.noreply.github.com>
2024-02-26 22:16:27 -0300

    Teste de Alteracao
    Feita alteracao nos arquivos, meu primeiro teste

---
+++ ArquiteturaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Suporte_TI_Garcia.Shared.Models;
+
+
+namespace Suporte_TI_Garcia.Shared
+{
+   
+
+    public class ArquiteturaService
+    {
+       public List<Arquitetura> ArquiteturaList { get; set; } = new List<Arquitetura>();
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Suporte_TI_Garcia.Client.Pages;
+using Suporte_TI_Garcia.Components;
+using Suporte_TI_Garcia.Shared;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddScoped<ArquiteturaService>();
+builder.Services.AddSingleton<ArquiteturaService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Suporte_TI_Garcia.Client._Imports).Assembly);
+
+app.Run();
